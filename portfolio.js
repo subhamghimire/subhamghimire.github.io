47d29d2fe828cd99d8baa0e424e0267f98a675ba
subhamghimire <shekhar.ghimire24@gmail.com>
2022-11-25 13:41:06 +0545

    Added new experience

--- portfolio.js
+++ portfolio.js
@@ -1,70 +1,78 @@
-import emoji from "react-easy-emoji";
+import emoji from 'react-easy-emoji';
 
 export const greetings = {
-  name: "Shekhar Ghimire",
+  name: 'Shekhar Ghimire',
   title: "Hi all, I'm Shekhar",
   description:
     "I'm passionate Full Stack web developer having an experience of web applications with PHP, Laravel, Vue.js, React.js",
-  resumeLink: "https://subhamghimire.github.io/resume",
+  resumeLink: 'https://subhamghimire.github.io/resume',
 };
 
 export const openSource = {
-  githubUserName: "subhamghimire",
+  githubUserName: 'subhamghimire',
 };
 
 export const contact = {};
 
 export const socialLinks = {
-  facebook: "https://www.facebook.com/profile.php?id=100069337756812",
-  instagram: "https://www.instagram.com/subham.ghimire/",
-  twitter: "https://twitter.com/GhimireShekhar1",
-  github: "https://github.com/subhamghimire",
-  linkedin: "https://www.linkedin.com/in/shekhar-ghimire-0944a4157",
+  facebook: 'https://www.facebook.com/profile.php?id=100069337756812',
+  instagram: 'https://www.instagram.com/subham.ghimire/',
+  twitter: 'https://twitter.com/GhimireShekhar1',
+  github: 'https://github.com/subhamghimire',
+  linkedin: 'https://www.linkedin.com/in/shekhar-ghimire-0944a4157',
 };
 
 export const skillsSection = {
-  title: "What I do",
-  subTitle: "FULL STACK DEVELOPER",
+  title: 'What I do',
+  subTitle: 'FULL STACK DEVELOPER',
   data: [
     {
-      title: "Full Stack Development",
-      lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+      title: 'Full Stack Development',
+      lottieAnimationFile: '/lottie/skills/fullstack.json', // Path of Lottie Animation JSON File
       skills: [
-        emoji("⚡ Experience with Laravel, MySql, Vue js on business domains"),
-        emoji(
-          "⚡ Experience with REST APIs with understanding of object-oriented programming (OOP) design patterns"
-        ),
-        emoji("⚡ Integration & Unit Testing"),
-        emoji("⚡ Experience working with Agile and Scrum teams"),
+        emoji('⚡ Experience with Laravel + Vue JS, Angular, React, NestJs, MEAN/MERN Stack on business domains'),
+        emoji('⚡ Experience with REST APIs with understanding of object-oriented programming (OOP) design patterns'),
+        emoji('⚡ Integration & Unit Testing'),
+        emoji('⚡ Docker, CI/CD pipeline'),
+        emoji('⚡ Experience working with Agile and Scrum teams'),
+        emoji('⚡ Learning ML'),
       ],
       softwareSkills: [
         {
-          skillName: "HTML-5",
-          fontAwesomeClassname: "vscode-icons:file-type-html",
+          skillName: 'HTML-5',
+          fontAwesomeClassname: 'vscode-icons:file-type-html',
         },
         {
-          skillName: "CSS-3",
-          fontAwesomeClassname: "vscode-icons:file-type-css",
+          skillName: 'CSS-3',
+          fontAwesomeClassname: 'vscode-icons:file-type-css',
         },
         {
-          skillName: "JavaScript",
-          fontAwesomeClassname: "logos:javascript",
+          skillName: 'JavaScript',
+          fontAwesomeClassname: 'logos:javascript',
         },
         {
-          skillName: "PHP",
-          fontAwesomeClassname: "logos:php",
+          skillName: 'PHP',
+          fontAwesomeClassname: 'logos:php',
         },
         {
-          skillName: "Laravel",
-          fontAwesomeClassname: "logos:laravel",
+          skillName: 'Laravel',
+          fontAwesomeClassname: 'logos:laravel',
         },
         {
-          skillName: "Vuejs",
-          fontAwesomeClassname: "vscode-icons:file-type-vue",
+          skillName: 'VueJS',
+          fontAwesomeClassname: 'vscode-icons:file-type-vue',
         },
         {
-          skillName: "Reactjs",
-          fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+          skillName: 'Angular',
+          fontAwesomeClassname: 'vscode-icons:file-type-angular',
+        },
+        {
+          skillName: 'NestJS',
+          fontAwesomeClassname: 'vscode-icons:file-type-nestjs',
+        },
+        {
+          skillName: 'ReactJS',
+          fontAwesomeClassname: 'vscode-icons:file-type-reactjs',
         },
       ],
     },
@@ -73,30 +81,37 @@ export const skillsSection = {
 
 export const educationInfo = [
   {
-    schoolName: "Tribhuvan University",
-    subHeader: "CSIT",
-    duration: "April 2018 - April 2022",
+    schoolName: 'Tribhuvan University',
+    subHeader: 'CSIT',
+    duration: 'April 2018 - Oct 2022',
   },
   {
-    schoolName: "Hetauda School of Management & Social Sciences",
-    subHeader: "Science",
-    duration: "April 2015 -  September 2017",
+    schoolName: 'Hetauda School of Management & Social Sciences',
+    subHeader: 'Science',
+    duration: 'April 2015 -  September 2017',
   },
 ];
 
 export const experience = [
   {
-    role: "Full Stack Developer",
-    company: "Alphatech",
-    companylogo: "/img/icons/common/alphatech.png",
-    date: "Dec 2019 - Present",
-    desc: "Working as Full Stack web developer to create fast, user-centric web apps, clean maintainable code & intuitive user interfaces",
+    role: 'MEAN Stack Developer',
+    company: 'Kilroy Blockchain',
+    companylogo: '/img/icons/common/kilroyblockchain-logo.jpg',
+    date: 'Dec 2019 - June 2022',
+    desc: 'Working on web, AI and blockchain projects with team as a developer',
   },
   {
-    role: "Junior Web Developer",
-    company: "Rubix Web",
-    companylogo: "/img/icons/common/rubix.png",
-    date: "Sept 2018 - Dec 2019",
-    desc: "Learned PHP, Laravel framework development along with frontend technologies",
+    role: 'Full Stack Developer',
+    company: 'Alphatech',
+    companylogo: '/img/icons/common/alphatech.png',
+    date: 'Dec 2019 - June 2022',
+    desc: 'Worked as Full Stack web developer to create fast, user-centric web apps,maintainable code & intuitive user interfaces',
+  },
+  {
+    role: 'Junior Web Developer',
+    company: 'Rubix Web',
+    companylogo: '/img/icons/common/rubix.png',
+    date: 'Sept 2018 - Dec 2019',
+    desc: 'Learned PHP, Laravel framework development along with frontend technologies',
   },
 ];
