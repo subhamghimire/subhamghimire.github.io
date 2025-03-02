23e04f60c62a4fbd1eeef64df5ffe904c14828e7
Shekhar Ghimire <shekhar.ghimire24@gmail.com>
2025-03-02 16:45:41 +0545

    Update DisplayLottie.jsx

--- components/DisplayLottie.jsx
+++ components/DisplayLottie.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
+
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 const GreetingLottie = ({ animationPath }) => {
 	const defaultOptions = {
