ab347a79786960307134bd2b46a1b580ad5f318d
Shekhar Ghimire <shekhar.ghimire24@gmail.com>
2026-05-01 13:36:19 +0545

    Fix yml file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
